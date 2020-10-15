9988953ad2db1e2037263625bfa2ee0bc3dde028
Nicholas Novak <nicholasmnovak@gmail.com>
2020-10-15 13:19:13 -0700

    Modified the flask build to redirect to the document root

--- config.py
+++ config.py
@@ -2,3 +2,5 @@ import os
 
 class Config(object):
     TEMPLATES_AUTO_RELOAD = os.environ.get('TEMPLATES_AUTO_RELOAD') or 'True' # Makes sure the templates reload when we make a change in development
+    FREEZER_DESTINATION = os.environ.get('FREEZER_DESTINATION') or '../'
+    FREEZER_REMOVE_EXTRA_FILES = os.environ.get('FREEZER_REMOVE_EXTRA_FILES') or 'False'
